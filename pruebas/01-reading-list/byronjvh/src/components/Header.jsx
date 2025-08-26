aafef30c0a714151ff9f94013eab2d6a4f5697c7
byronjvh <byronjvh02@gmail.com>
2025-08-26 13:04:10 -0600

    Added GitHub link

--- pruebas/01-reading-list/byronjvh/src/components/Header.jsx
+++ pruebas/01-reading-list/byronjvh/src/components/Header.jsx
@@ -5,6 +5,7 @@ import { GlobalContext } from '../context/GlobalContext.jsx'
 import { Tag } from './Tag'
 import { ReadingList } from './ReadingList'
 import { Counter } from './Counter'
+import { GitHub } from './Icons.jsx'
 
 export function Header () {
   const { readingList, availableBooks, filteredBooks, getSelectedGenre, selectedGenre } = useContext(GlobalContext)
@@ -13,7 +14,12 @@ export function Header () {
     <header className='relative z-10 text-sky-950 flex flex-col items-center'>
       <div className='w-full flex justify-between gap-3 p-4 xl:px-32 mb-8 bg-white'>
         <Search />
-        <ReadingList />
+        <div className='flex items-center gap-8'>
+          <a href="https://pruebas-tecnicas-murex.vercel.app/" rel='noopener noreferer' className='flex items-center gap-1 text-sky-800 hover:brightness-150'>
+            Visit GitHub <GitHub size="24" className="" />
+          </a>
+          <ReadingList />
+        </div>
       </div>
       <div className='w-full max-w-[1000px] py-4 px-3 flex flex-col gap-4'>
         <div className='flex justify-between items-center'>
